5d288d7b6248cdda4e63d11e0c622658e1b74502
Gunther Cox <gunthercx@gmail.com>
2015-01-03 14:56:38 -0500

    Updated speech synthesis code to work with serial connection.

--- speech_synthesis/speech_synthesis.ino
+++ speech_synthesis/speech_synthesis.ino
@@ -1,80 +1,45 @@
-/*
-  Text To Speech syntesis library
- Copyright (c) 2008 Clive Webster.  All rights reserved.
- Nov. 29th 2009 - Modified to work with Arduino by Gabriel Petrut.
- The Text To Speech library uses Timer1 to generate the PWM
- output on digital pin 10. The output signal needs to be fed
- to an RC filter then through an amplifier to the speaker.
- */
-
-#include <Wire.h>
 #include "TTS.h"
 
-char text [50];
-
-// String to store text which needs to be spoken
-String buffer = "";
+// The name of this board used to identify it on the serial port
+const String NAME = "text_to_speech";
 
 // speech output is digital pin 10
-TTS text2speech;
+TTS tts;
 
-void Test_Speech(char input []) {
-  // higher values = lower voice pitch
-  text2speech.setPitch(16);
-  
-  strcpy(text, input);
-  text2speech.sayText(text);
-}
-
-// This event function executes whenever data is received from master
-void receiveEvent(int byteCount) {
-  while(Wire.available())
-  {
-    // receive a byte as character
-    char c = Wire.read();
-    buffer += c;
-  }
-}
+//char input[8] = {'a', 'r', 'd', 'u', 'i', 'n', 'o'};
+char input[8];
+int index = 0;
 
 void setup() {
-  
-  Serial.begin(9600);
-  Serial.println("Slave online");
-  
-  // join i2c bus with address #4
-  Wire.begin(4);
-  
-  // register event
-  Wire.onReceive(receiveEvent);
+  // higher values = lower voice pitch
+  tts.setPitch(16);
+
+  Serial.begin(115200);
 }
+
+char line[80];
+int pos = 0;
 
 void loop() {
-  
-  // Overrid the serial loop to test
-  while (true) {
-    text2speech.setPitch(16);
-    text2speech.sayText("Hello, Salvius is standing by");
-    
-      delay(8000);
 
-  }
+  if (Serial.available() > 0) {
 
-  while(Wire.available() == false) {
-  
-  Serial.print(buffer);
-    if (buffer != "") {
-      // Convert string to char array
-      char item[50];
-      buffer.toCharArray(item, 50);
-      
-      Test_Speech(item);
-      
-      // The text has been spoken so clear the buffer
-      buffer = "";
+    Serial.println(NAME);
+
+    char c = (char)Serial.read();
+    if (c == '\n') {
+      line[pos++] = 0;
+      if (line[0] != '+') {
+        Serial.println(line);
+        tts.sayText(line);
+      }
+      pos = 0;
+    } else {
+      line[pos++] = c;
     }
-  }
-  
-  delay(1000);
-}
 
+  }
+
+  delay(100);
+}
 
