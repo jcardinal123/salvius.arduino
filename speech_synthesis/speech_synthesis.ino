ef672bd323d764f5a3ecbbc001d5693f22c3a8f0
Gunther Cox <gunthercx@gmail.com>
2015-01-17 10:56:09 -0500

    Added board name for test to speech.

--- speech_synthesis/speech_synthesis.ino
+++ speech_synthesis/speech_synthesis.ino
@@ -66,18 +66,23 @@ void loop() {
 
 /*
   SerialEvent occurs whenever a new data comes in the
- hardware serial RX.  This routine is run between each
+ hardware serial RX. This routine is run between each
  time loop() runs, so using delay inside loop can delay
- response.  Multiple bytes of data may be available.
+ response. Multiple bytes of data may be available.
  */
 void serialEvent() {
   while (Serial.available()) {
     // get the new byte:
-    char inChar = (char)Serial.read(); 
-    // add it to the inputString:
-    inputString += inChar;
+    char inChar = (char)Serial.read();
+
     // if the incoming character is a newline, set a flag
     // so the main loop can do something about it:
+    if (inChar == ':') {
+       Serial.println(NAME);
+    } else {
+       inputString += inChar;
+    }
+
     if (inChar == '\n') {
       stringComplete = true;
     } 
