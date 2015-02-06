aa21116bc1434632ca163bb7872b247572929e22
Gunther Cox <gunthercx@gmail.com>
2015-02-06 18:43:49 -0500

    Updated code to recieve serial data.

--- neck_servo/neck_servo.ino
+++ neck_servo/neck_servo.ino
@@ -11,35 +11,37 @@ void setup() {
   //servo1.setMaximumPulse(2000);
   //servo1.setMinimumPulse(700);
 
-  Serial.begin(115200);
+  Serial.begin(9600);
 }
 
 void loop() {
   // Read serial input:
-  while (Serial.available() > 0) {
+  while (Serial.available()) {
     int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char 
-      // and add it to the string:
-      inString += (char)inChar; 
+
+    //if (isDigit(inChar)) {
+    if (inChar >= '0' && inChar <= '9') {
+      // convert the incoming byte to a char and add it to the string:
+      inString += (char)inChar;
     }
+    
+      if (inChar == ':') {
+        // Print the board name to identify with python code
+        Serial.println(NAME);
+      }
 
-    if (inChar == ' ') {
-      // Print the board name to identify with python code
-      Serial.println(NAME);
-    }
+      if (inChar == '\n') {
+        // if you get a newline, print the string, then the string's value:
+        Serial.println(inString);
+        servo1.write(inString.toInt());
+        // clear the string for new input
+        inString = "";
+      }
 
-    // if you get a newline, print the string,
-    // then the string's value:
-    if (inChar == '\n') {
-      Serial.println(inString);
-
-      servo1.write(inString.toInt());
-
-      // clear the string for new input
-      inString = ""; 
-    }
   }
 }
+
+
+
 
 
